92b003ebcb41e0dea1a27c50db6038f570e2e4f3
M1roel <ppfautsch@yahoo.de>
2024-06-09 15:32:35 +0200

    Post Container angepasst: Gefällt mit Angaben, Author Comments, Benutzer comments, Datum des Postes

--- src/js/data.js
+++ src/js/data.js
@@ -1,10 +1,27 @@
 let data = [
-    {
-        'authorimg': '../../public/img/Ich.jpg',
-        'author': 'Tagesschau',
-        'image': '../../public/img/img1.png',
-        'description': 'Weiterhin starke Überschwemmungen in den Risikogebieten.',
-        'likes': '',
-        'date': ''
-    }
+  {
+    authorimg: "../../public/img/Ich.jpg",
+    author: "tagesschau",
+    image: "../../public/img/img1.png",
+    description: "Weiterhin starke Überschwemmungen in den Risikogebieten.",
+    wholike: "peter",
+    likes: "5",
+    comments: [
+      { poster: "peter", comment: "Schlimm, was da passiert ist" },
+      { poster: "anna", comment: "Ja, wirklich traurig" },
+      { poster: "maria", comment: "Ich hoffe, es geht allen gut" },
+    ],
+    date: "09.06.2024",
+  },
+  {
+    authorimg: "../../public/img/Ich.jpg",
+    author: "Tagesschau",
+    image: "../../public/img/img1.png",
+    description: "Weiterhin starke Überschwemmungen in den Risikogebieten.",
+    wholike: "",
+    likes: "",
+    comments: "",
+    likes: "",
+    date: "",
+  },
 ];
--- src/js/post.js
+++ src/js/post.js
@@ -1,19 +1,28 @@
 let posts = data;
 
 function show() {
-    document.getElementById('postcontainer').innerHTML += '';
+    document.getElementById('postcontainer').innerHTML = '';
 
     for (let i = 0; i < posts.length; i++) {
         const post = posts[i];
 
+        let commentsHtml = '';
+        for (let j = 0; j < post.comments.length; j++) {
+            const comment = post.comments[j];
+            commentsHtml += `<div class="comment"><b>${comment.poster}</b> ${comment.comment}</div>`;
+        }
+
         document.getElementById('postcontainer').innerHTML += `
-        <div>
+        <div class="post">
             <div class="authorinfo">
-            <img src="${post['authorimg']}" id="authorimg">
-            <div>${post['author']}</div>
+                <img src="${post.authorimg}" id="authorimg">
+                <div>${post.author}</div>
             </div>
-            <img src="${post['image']}">
-            <div>${post['description']}</div>
+            <img src="${post.image}">
+            <div class="likes">Gefällt ${post.wholike} und ${post.likes} weiteren Personen</div>
+            <div class="authorcomment"><b>${post.author}</b> ${post.description}</div>
+            <div class="comments">${commentsHtml}</div>
+            <div class="date">${post.date}</div>
         </div>
         `;        
     }
