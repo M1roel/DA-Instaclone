c67a0515ee11e7d1797db635020b76c60efdaf0e
M1roel <ppfautsch@yahoo.de>
2024-06-09 15:44:13 +0200

    Datenstruktur angepasst

--- src/js/data.js
+++ src/js/data.js
@@ -1,9 +1,26 @@
 let data = [
   {
     authorimg: "../../public/img/Ich.jpg",
+    author: "summerbreez",
+    image: "../../public/img/img2.png",
+    description:
+      "Der Sommer wird heiß Leute! Sonne, Pool und Cocktails! Was brauchen wir da mehr?",
+    wholike: "vianna",
+    likes: "249",
+    comments: [
+      { poster: "peter", comment: "PARTY!" },
+      { poster: "kerstin", comment: "Ja, der Pool ist schon bereit!" },
+      { poster: "irene", comment: "Wir bringen die Cocktails mit :)" },
+      { poster: "vianna", comment: "Felix und ich kommen auch! :D" }
+    ],
+    date: "09.06.2024",
+  },
+  {
+    authorimg: "../../public/img/Ich.jpg",
     author: "tagesschau",
     image: "../../public/img/img1.png",
-    description: "Weiterhin starke Überschwemmungen in den Risikogebieten.",
+    description:
+      "Weiterhin starke Überschwemmungen in den Risikogebieten. Aber das THW ist bereits unterwegs und erste Helfer haben schon mit den Aufräumarbeiten begonnen",
     wholike: "peter",
     likes: "5",
     comments: [
@@ -12,16 +29,5 @@ let data = [
       { poster: "maria", comment: "Ich hoffe, es geht allen gut" },
     ],
     date: "09.06.2024",
-  },
-  {
-    authorimg: "../../public/img/Ich.jpg",
-    author: "Tagesschau",
-    image: "../../public/img/img1.png",
-    description: "Weiterhin starke Überschwemmungen in den Risikogebieten.",
-    wholike: "",
-    likes: "",
-    comments: "",
-    likes: "",
-    date: "",
   },
 ];
