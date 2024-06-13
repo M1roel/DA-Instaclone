6538db10545f0da1d57665f5786319128253da34
M1roel <157500795+M1roel@users.noreply.github.com>
2024-06-13 13:43:07 +0200

    Kommentieren ist jetzt moeglich

--- src/js/post.js
+++ src/js/post.js
@@ -27,21 +27,23 @@ function generateStaticImagesHtml(index) {
 
 /* Funktion um beim Klick auf das Herz das Image zu ändern */
 function toggleHeart(index) {
-    const postHeart = document.getElementById(`post-heart-${index}`);
-    if (postHeart.src.includes("heart.png")) {
-      postHeart.src = "/public/img/heart-red.png";
-      countUp(index);
-    } else {
-      postHeart.src = "/public/img/heart.png";
-      countDown(index);
-    }
+  const postHeart = document.getElementById(`post-heart-${index}`);
+  if (postHeart.src.includes("heart.png")) {
+    postHeart.src = "/public/img/heart-red.png";
+    countUp(index);
+  } else {
+    postHeart.src = "/public/img/heart.png";
+    countDown(index);
+  }
 }
 
 /* Funktion um die Likes zu erhöhen */
 function countUp(index) {
   let likesElement = document.getElementById(`likes-${index}`);
   let currentLikes = parseInt(likesElement.textContent.match(/\d+/)[0], 10);
-  likesElement.textContent = `Gefällt ${posts[index].wholike} und ${currentLikes + 1} weiteren Personen`;
+  likesElement.textContent = `Gefällt ${posts[index].wholike} und ${
+    currentLikes + 1
+  } weiteren Personen`;
   posts[index].likes = currentLikes + 1;
 }
 
@@ -49,18 +51,31 @@ function countUp(index) {
 function countDown(index) {
   let likesElement = document.getElementById(`likes-${index}`);
   let currentLikes = parseInt(likesElement.textContent.match(/\d+/)[0], 10);
-  likesElement.textContent = `Gefällt ${posts[index].wholike} und ${currentLikes - 1} weiteren Personen`;
+  likesElement.textContent = `Gefällt ${posts[index].wholike} und ${
+    currentLikes - 1
+  } weiteren Personen`;
   posts[index].likes = currentLikes - 1;
 }
 
 /* Funktion um die Kommentare für die Posts zu rendern */
 function generateCommentsHtml(comments) {
-  let commentsHtml = '';
+  let commentsHtml = "";
   for (let i = 0; i < comments.length; i++) {
     const comment = comments[i];
     commentsHtml += `<div class="comment"><b>${comment.poster}</b> ${comment.comment}</div>`;
   }
   return commentsHtml;
+}
+
+/* Funktion zum Posten eines neuen Kommentars */
+function postComment(index) {
+  const commentInput = document.getElementById(`comment-input-${index}`);
+  const newComment = commentInput.value;
+  if (newComment) {
+    posts[index].comments.push({ poster: "peter", comment: newComment });
+    commentInput.value = "";
+    show();
+  }
 }
 
 /* Funktion um die einzelnen Post-Container zu rendern */
@@ -77,8 +92,8 @@ function generatePostContainer(post, commentsHtml, staticImagesHtml, index) {
         <div class="authorcomment"><b>${post.author}</b> ${post.description}</div>
         <div class="comments">${commentsHtml}</div>
         <div class="writepost">
-          <input type="text" placeholder="Kommentieren..." />
-          <button>Posten</button>
+          <input type="text" id="comment-input-${index}" placeholder="Kommentieren..." />
+          <button onclick="postComment(${index})">Posten</button>
         </div>
     </div>
   `;
@@ -86,14 +101,19 @@ function generatePostContainer(post, commentsHtml, staticImagesHtml, index) {
 
 /* Funktion um den gesamten Post-Container zu rendern */
 function show() {
-  document.getElementById("postcontainer").innerHTML = '';
+  document.getElementById("postcontainer").innerHTML = "";
 
   for (let i = 0; i < posts.length; i++) {
     const post = posts[i];
 
     let commentsHtml = generateCommentsHtml(post.comments);
     let staticImagesHtml = generateStaticImagesHtml(i);
-    
-    document.getElementById("postcontainer").innerHTML += generatePostContainer(post, commentsHtml, staticImagesHtml, i);
+
+    document.getElementById("postcontainer").innerHTML += generatePostContainer(
+      post,
+      commentsHtml,
+      staticImagesHtml,
+      i
+    );
   }
 }
